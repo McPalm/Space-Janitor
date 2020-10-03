1a3e83b9db8f7581ce8ffe363458e27a17ac61a6
palm <tauman.exe@gmail.com>
2020-10-03 04:06:45 +0200

    Rudementary first person controller.

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public float yaw;
+    float pitch;
+
+    bool invertY = false;
+
+    public float mouseSensitivityFactor = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pitch = transform.eulerAngles.x;
+        yaw = transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
+
+        // var mouseSensitivityFactor = 1f; // mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
+
+        yaw += mouseMovement.x * mouseSensitivityFactor;
+        pitch += mouseMovement.y * mouseSensitivityFactor;
+        pitch = Mathf.Clamp(pitch, -80f, 80f);
+
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
+
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Transform Camera;
+    public Rigidbody Rigidbody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = GetInputTranslationDirection();
+        if(direction.z != 0f)
+        {
+            Rigidbody.AddForce(Camera.forward * direction.z);
+        }
+        if(direction.x != 0f)
+        {
+            Rigidbody.AddForce(Camera.right * direction.x);
+        }
+    }
+
+    Vector3 GetInputTranslationDirection()
+    {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        return direction;
+    }
+}
