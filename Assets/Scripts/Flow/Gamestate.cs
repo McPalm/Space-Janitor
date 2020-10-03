7934a2da697e68c3beb1e7c4b4f5b5ab60cfeef3
palm <tauman.exe@gmail.com>
2020-10-03 21:11:10 +0200

    Spawns trash in an designated areas.

---
+++ Assets/Scripts/Flow/Gamestate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamestate : MonoBehaviour
+{
+    public BussGenerator BussGenerator;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        int count = 0;
+    start:
+        Debug.Log("Start!");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("1");
+        BussGenerator.trashNumber = 5 + count;
+        SpawnTrash();
+        count++;
+        Debug.Log("2");
+        yield return new WaitForSeconds(30f);
+    evaluate:
+        Debug.Log("TIME!!!");
+        Debug.Log($"you missed {BussGenerator.MissedTrash()} objects!");
+        yield return new WaitForSeconds(1f);
+        BussGenerator.Reset();
+        goto start;
+    }
+
+    void SpawnTrash()
+    {
+        BussGenerator.Generate();
+    }
+}
---
+++ Assets/Scripts/Systems/BussGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BussGenerator : MonoBehaviour
+{
+    public GameObject[] TrashPrefabs;
+    public BoxCollider SpawnZone;
+
+    Transform MockPoint;
+
+    public int trashNumber;
+
+    List<GameObject> spawnedTrash;
+
+    private void Awake()
+    {
+        MockPoint = new GameObject().transform;
+        MockPoint.SetParent(SpawnZone.transform);
+        spawnedTrash = new List<GameObject>();
+    }
+
+    public void Generate()
+    {
+        Debug.Log($"Generating {trashNumber} trash!");
+        for (int i = 0; i < trashNumber; i++)
+        {
+            var point = GetRandomPoint();
+            var fab = TrashPrefabs[Random.Range(0, TrashPrefabs.Length)];
+            var obj = Instantiate(fab, GetRandomPoint(), Quaternion.identity); // TODO randomize the rotation probably.
+            spawnedTrash.Add(obj);
+        }
+    }
+
+    public int MissedTrash()
+    {
+        int miss = 0;
+        foreach(var trash in spawnedTrash)
+        {
+            if (trash != null)
+                miss++;
+        }
+        return miss;
+    }
+
+    public void Reset()
+    {
+        foreach(var trash in spawnedTrash)
+        {
+            if (trash != null)
+                Destroy(trash);
+        }
+        spawnedTrash.Clear();
+    }
+
+    Vector3 GetRandomPoint()
+    {
+        var local = new Vector3(
+            (.5f - Random.value) * SpawnZone.size.x,
+            (.5f - Random.value) * SpawnZone.size.y,
+            (.5f - Random.value) * SpawnZone.size.z);
+        MockPoint.localPosition = local;
+        return MockPoint.position;
+    }
+}
