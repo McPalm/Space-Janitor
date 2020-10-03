8cb23c14cd14194bfeaff4f6ad4c57dbf2c1afbb
palm <tauman.exe@gmail.com>
2020-10-03 18:21:44 +0200

    Added crouching.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,16 +8,24 @@ public class Move : MonoBehaviour
     public Transform Camera;
     public CharacterController CharacterController;
     public float speed = 5f;
+    public float crouchSpeed = 2f;
     public float acceleration = .2f;
     Vector3 lastmove;
 
+    public float crouch;
 
+    public float crouchDelta = .5f;
+
+    Vector3 cameraStartPosition;
+
+    float CalculatedSpeed => Mathf.Lerp(speed, crouchSpeed, crouch);
 
     // Update is called once per frame
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined; //lock dat cursor
+        cameraStartPosition = Camera.localPosition;
     }
 
     void Update() //Noticed a stutter I think was due to using Fixed Update So I changed this to Update. 
@@ -26,13 +34,22 @@ public class Move : MonoBehaviour
         var direction = GetInputTranslationDirection();
         Vector3 move = Camera.forward * direction.z + Camera.right * direction.x;
         move.y = 0f;
-        lastmove = move * acceleration + lastmove * (1f- acceleration);
-        if(CharacterController.isGrounded)
+        lastmove = move * acceleration + lastmove * (1f - acceleration);
+        if (CharacterController.isGrounded)
             lastmove.y = 0f;
         else
             lastmove.y -= 9.98f * Time.deltaTime;
-        CharacterController.Move(lastmove * speed * Time.deltaTime);
+        CharacterController.Move(lastmove * CalculatedSpeed * Time.deltaTime);
+        HandleCrouch();
+    }
 
+    void HandleCrouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+            crouch = Mathf.Lerp(crouch, 1f, Time.deltaTime * 5f);
+        else
+            crouch = Mathf.Lerp(crouch, 0f, Time.deltaTime * 5f);
+        Camera.localPosition = cameraStartPosition + Vector3.down * crouchDelta * crouch;
     }
 
     Vector3 GetInputTranslationDirection()
