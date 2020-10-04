0371f42b6d9701d310e0449f245a12adc62468d4
palm <tauman.exe@gmail.com>
2020-10-05 00:41:21 +0200

    +Broom.

---
+++ Assets/Scripts/MirrorRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorRotation : MonoBehaviour
+{
+
+    public Transform original;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0f, original.eulerAngles.y, 0f);
+    }
+}
