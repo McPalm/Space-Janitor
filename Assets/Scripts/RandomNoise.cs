6466d6ab9bc56ae6bd0df592adee949b7f94b437
palm <tauman.exe@gmail.com>
2020-10-05 19:25:48 +0200

    Random step sounds.

---
+++ Assets/Scripts/RandomNoise.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNoise : MonoBehaviour
+{
+    public AudioClip[] noises;
+
+    public AudioSource AudioSource;
+    // Start is called before the first frame update
+    public void Play()
+    {
+        AudioSource.clip = noises[Random.Range(0, noises.Length)];
+        AudioSource.Play();
+    }
+}
